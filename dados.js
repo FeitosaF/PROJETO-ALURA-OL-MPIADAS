7578c7a0ccbd21dbc8ef53bf04e422ad28de18e8
Feitosa Filho <feitosa_adm@outlook.com>
2024-09-09 10:37:26 -0300

    Projeto alura olimpiadas

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: 'Rebeca Andrade',
+        descricao: 'É uma ginasta artística brasileira bicampeã olímpica e a maior medalhista da história do Brasil nas Olimpíadas, com 6 medalhas (2 ouros, 3 pratas e 1 bronze).',
+        tags: "ginástica ginastica ouro" // Palavras chaves para pesquisa no imput
+    },
+
+    {
+        titulo: 'Rayssa Leal',
+        descricao: 'É uma skatista brasileira, vice-campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, sendo a mais jovem medalhista olímpica brasileira.',
+        link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+        tags: "skate fada sk8"
+     },
+
+    {
+       titulo: 'Beatriz Sousa',
+       descricao: 'É uma judoca, sargento do exército e campeã olímpica brasileira. Beatriz conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris e com isso se tornou a primeira mulher a conquistar uma medalha de ouro Olímpica na história do Esporte Clube Pinheiros.',
+       link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+       tags: "judô judo judoca judoka "
+    },
+      
+];
---
+++ script.js
@@ -0,0 +1,49 @@
+// O resultado no OUTPUT do VSCode está dando erro por causa que não está na mesma página.
+// O resultado só aparece no console do navegador.
+/*console.log(dados);
+console.log(dados[1]);
+console.log(dados[0].descricao);*/
+
+function pesquisar(){
+  let section = document.getElementById ('atletas');
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+if (!campoPesquisa) { // Condiçao como campo de busca vazio
+  section.innerHTML = "<p> Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte </p>";
+  return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase();
+
+  let resultados = ""; // String vazia para armazenar os resultados
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+  
+  // Para cada dado da lista de dados faz uma alteração
+  for (let dado of dados){ // O for coloca todos os elementos
+    
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      resultados += ` 
+      <div class="atletas-resultados">
+      <h2> <a href="#" target="_blank"> ${dado.titulo} </a> </h2>
+          <p class="descricao"> ${dado.descricao} </p>
+          <p class="descricaoa"> <a href= ${dado.link} target="_blank"> Mais informações </a> </p>
+    </div>
+  `;
+    }
+      
+  };
+
+  if (!resultados) {
+    resultados = "<p> Nada foi encontrado </p>";
+  }
+  section.innerHTML = resultados;
+ 
+} 
+
